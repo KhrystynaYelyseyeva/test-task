9d7c1dac670e6cd10129dfc0ff9f3bee424af005
Khrystyna Yelyseyeva <play1409@gmail.com>
2022-11-10 13:11:40 +0200

    add react query

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Home, Help } from "./views";
 import App from "./App";
 import "./index.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
     <React.StrictMode>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="help" element={<Help />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="help" element={<Help />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </React.StrictMode>
   </HashRouter>
 );
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,33 +1,24 @@
-import { useEffect, useState, memo } from "react";
+import { memo } from "react";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Typography from "@mui/material/Typography";
+import { useQuery } from "@tanstack/react-query";
+
+const getData = async () => {
+  const response = await fetch(
+    "https://api.thecatapi.com/v1/images/search?limit=10"
+  );
+
+  return await response.json();
+};
 
 const Home = () => {
-  const [data, setData] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await fetch(
-          "https://api.thecatapi.com/v1/images/search?limit=10"
-        );
-        const result = await response.json();
-
-        setData(result);
-      } catch (error) {
-        setError(error);
-      }
-    };
-
-    getData();
-  }, []);
+  const { data = [], error, isLoading } = useQuery(["data"], getData);
 
   return (
     <Box sx={{ width: "100%" }} data-testid="home-page">
-      {!!error ? (
+      {!!error || isLoading ? (
         <Typography variant="h4" align="center" sx={{ my: 3 }}>
           No data
         </Typography>
@@ -37,17 +28,16 @@ const Home = () => {
             Don't you have a cat yet?
           </Typography>
           <ImageList variant="masonry" cols={3} gap={8}>
-            {data.length > 0 &&
-              data.map(item => (
-                <ImageListItem key={item.id}>
-                  <img
-                    src={`${item.url}?w=248&fit=crop&auto=format`}
-                    srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                    alt="cat"
-                    loading="lazy"
-                  />
-                </ImageListItem>
-              ))}
+            {data.map(item => (
+              <ImageListItem key={item.id}>
+                <img
+                  src={`${item.url}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt="cat"
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
           </ImageList>
         </>
       )}
